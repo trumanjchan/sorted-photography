b10c2c18d451b346544db717205ef70276214da4
Truman <trumanjfchan@gmail.com>
2024-09-26 13:42:55 -0700

    feat: use original file name to avoid duplicate date_time format. Also add camera make and model to naming convention.

--- index.js
+++ index.js
@@ -5,30 +5,20 @@ var ExifImage = require('exif').ExifImage;
 
 console.log("Start: " + files.length);
 
-const templetter = ["a", "b", "c", "d"];  //needs work
-
 for (let i = 0; i < files.length; i++) {
     try {
         new ExifImage({ image : folderPath + files[i] }, function (error, exifData) {
             if (error)
                 return;
             else
-                console.log(exifData.exif.DateTimeOriginal);
                 var date = exifData.exif.DateTimeOriginal.substring(0, 10).replaceAll(":", "");
                 var time = exifData.exif.DateTimeOriginal.substring(11, 16).replaceAll(":", "");
                 date = date.substring(4, 10) + date.substring(0, 4);
 
-                if (fs.existsSync(folderPath + date + '_' + time + '.JPG')) {  //needs work
-                    fs.renameSync(folderPath + files[i], folderPath + date + '_' + time + templetter[0] + '.JPG');
-                } else if (fs.existsSync(folderPath + date + '_' + time + templetter[0] + '.JPG')) {
-                    fs.renameSync(folderPath + files[i], folderPath + date + '_' + time + templetter[1] + '.JPG');
-                } else if (fs.existsSync(folderPath + date + '_' + time + templetter[1] + '.JPG')) {
-                    fs.renameSync(folderPath + files[i], folderPath + date + '_' + time + templetter[2] + '.JPG');
-                } else if (fs.existsSync(folderPath + date + '_' + time + templetter[2] + '.JPG')) {
-                    fs.renameSync(folderPath + files[i], folderPath + date + '_' + time + templetter[3] + '.JPG');
-                } else {
-                    fs.renameSync(folderPath + files[i], folderPath + date + '_' + time + '.JPG');
-                }
+                let originalName = files[i];
+                originalName = originalName.replace(/\.jpe?g/i, "");
+
+                fs.renameSync(folderPath + files[i], folderPath + date + '_' + time + '_' + originalName + '_' + exifData.image.Make + exifData.image.Model + '.jpg');
         });
     } catch (error) {
         console.log('Error: ' + error.message);
